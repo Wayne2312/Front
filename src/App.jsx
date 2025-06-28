031a16b7b2b8ea950f1ce326568147023d553544
Wayne2312 <Wayne.Kimani.805@gmail.com>
2025-06-28 22:50:32 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 import { AuthProvider } from "./contexts/AuthContext";
-import PrivateRoute from "./components/PrivateRoute";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Analysis from "./pages/Analysis";
-import Navbar from "./components/Navbar";
-import "./index.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <HelmetProvider>
-      <AuthProvider>
-        <Router>
-          <div className="min-h-screen bg-warm-beige font-sans">
-            <Navbar />
-            <main>
-              <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/" element={<Register />} /> {/* Set Register as default */}
-                <Route
-                  path="/dashboard"
-                  element={
-                    <PrivateRoute>
-                      <Dashboard />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/analysis"
-                  element={
-                    <PrivateRoute>
-                      <Analysis />
-                    </PrivateRoute>
-                  }
-                />
-              </Routes>
-            </main>
-          </div>
-        </Router>
-      </AuthProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/analysis" element={<Analysis />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
