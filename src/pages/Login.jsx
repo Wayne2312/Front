e26e590ceb3ebc2836413058039207e786bec190
Wayne2312 <Wayne.Kimani.805@gmail.com>
2025-06-28 23:31:04 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,8 +13,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await login(identifier, password);
-      navigate("/dashboard");
+      await login(identifier, password, navigate);
     } catch (err) {
       setError(err.message);
     }
